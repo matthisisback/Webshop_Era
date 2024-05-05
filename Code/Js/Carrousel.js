1e5c2e4829717e2e389e37f4ab8eb8ba3a3d3722
matthisisback <teplitxkymatthis@gmail.com>
2024-05-05 10:00:48 +0200

    Page de bienvenue finie sans les spé

--- Code/Js/Carrousel.js
+++ Code/Js/Carrousel.js
@@ -1,7 +1,35 @@
-document.querySelector('.fleche.gauche').addEventListener('click', function() {
-    document.querySelector('.images').style.transform = 'translateX(-100%)';
-});
+document.addEventListener("DOMContentLoaded", function() {
+    const images = ["img/Flopin.jpg", "img/Hao.jpg", "img/Udyr.jpg", "img/michel.jpg"];
+    const carouselImage = document.getElementById('carouselImage');
+    let currentIndex = 0;
 
-document.querySelector('.fleche.droite').addEventListener('click', function() {
-    document.querySelector('.images').style.transform = 'translateX(0)';
+    function showImage(index) {
+        carouselImage.src = images[index];
+    }
+
+    function prevImage() {
+        if (currentIndex > 0) {
+            currentIndex--;
+        } else {
+            currentIndex = images.length - 1;
+        }
+        showImage(currentIndex);
+    }
+
+    function nextImage() {
+        if (currentIndex < images.length - 1) {
+            currentIndex++;
+        } else {
+            currentIndex = 0;
+        }
+        showImage(currentIndex);
+    }
+
+    const prevBtn = document.querySelector('.prev-btn');
+    const nextBtn = document.querySelector('.next-btn');
+
+    prevBtn.addEventListener('click', prevImage);
+    nextBtn.addEventListener('click', nextImage);
+
+    showImage(currentIndex);
 });
